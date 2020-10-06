b298fb4a512a2688d69dfbd63cb7b716b16531fe
Jit Chanda <connects2jit@gmail.com>
2020-10-06 23:19:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//Import Routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true },
+ () => console.log('Connected to db...'));
+
+ //Middleware
+ app.use(express.json());
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('Server is up and running...'));
